66fead977df7da1241ecdb2d391aaab7904129a4
tutqq1803 <tutqq96@gmail.com>
2022-12-06 12:30:30 +0700

    Update resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Container, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_TruongQuangQuocTu.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+import pdf from "../../Assets/CV_TruongQuangQuocTu.pdf";
+import {AiOutlineDownload} from "react-icons/ai";
+import {Document, Page, pdfjs} from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
